9ea8bf09df73317b8a6607fcdd814e485edd5c53
Hemanth148909 <161000010+Hemanth148909@users.noreply.github.com>
2025-08-17 16:36:14 +0530

    Update main.tf

--- modules/storage_bucket/main.tf
+++ modules/storage_bucket/main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my_bucket" {
-  name          = "my-bucket-1489"  # must be globally unique
-  location      = "us-central1"
+  name          = var.bucket_name  # must be globally unique
+  location      = var.location
   
 }
